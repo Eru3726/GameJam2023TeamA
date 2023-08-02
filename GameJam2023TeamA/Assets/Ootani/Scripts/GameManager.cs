004949be5a2a118256d675ac499e355e4f832aa2
Eru <eru.gamecreater@gmail.com>
2023-08-02 14:17:22 +0900

    uiとプレイヤー消す

--- GameJam2023TeamA/Assets/Ootani/Scripts/GameManager.cs
+++ GameJam2023TeamA/Assets/Ootani/Scripts/GameManager.cs
@@ -7,15 +7,19 @@ public class GameManager : MonoBehaviour
     [SerializeField] GameOverPanelManager gameOverPanel;
     [SerializeField] Transform canvas;
 
+    [SerializeField]
+    private GameObject player,GameUI;
+
     int score = 1200;
     int distance=1111111;
-
 
     public void eatIkura()
     {
         gameOverPanel = Instantiate(gameOverPanel);
         gameOverPanel.transform.SetParent(canvas);
         gameOverPanel.GetComponent<RectTransform>().anchoredPosition = Vector3.zero;
+        player.SetActive(false);
+        GameUI.SetActive(false);
 
         gameOverPanel.setValue("’Ç‚¢•t‚©‚ê‚½", score, distance);
     }
