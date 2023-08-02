c8b211d508676a2e4c7cefcb218ee95345951fd1
DESKTOP-67MT2K8\user <ruiruitogemu@gmail.com>
2023-08-02 14:03:53 +0900

    ゲームオーバーパネル

--- GameJam2023TeamA/Assets/Ootani/Scripts/GameManager.cs
+++ GameJam2023TeamA/Assets/Ootani/Scripts/GameManager.cs
@@ -10,14 +10,9 @@ public class GameManager : MonoBehaviour
     int score = 1200;
     int distance=1111111;
 
-    private void Awake()
-    {
-
-    }
 
     public void eatIkura()
     {
-
         gameOverPanel = Instantiate(gameOverPanel);
         gameOverPanel.transform.SetParent(canvas);
         gameOverPanel.GetComponent<RectTransform>().anchoredPosition = Vector3.zero;
